0d7f5c14a5f24fcdc6781305720adc1280e93825
Phill93 <danielbacher@phill93.de>
2016-04-27 16:06:11 +0200

    fixed api

--- web/weather/api.php
+++ web/weather/api.php
@@ -1,12 +1,12 @@
 <?php
-  $db = mysqli_connect("localhost", "web", "web");
+  $db = mysqli_connect("localhost", "web", "web", "rpi_project");
 
-  if(mysqli_connect_errorno()) {
-    printf("Verbindung fehlgeschlagen: %s \n", mysqli_connect_errorno());
+  if(mysqli_connect_errno()) {
+    printf("Verbindung fehlgeschlagen: %s \n", mysqli_connect_errno());
     exit();
   }
 
-  $query = mysqli_query($db, "SELECT temp, hum, tstamp FROM temp, hum");
+  $query = mysqli_query($db, "select temps.temperature as temp, hums.humidity as hum from temps, hums where temps.tstamp < current_timestamp and temps.tstamp = hums.tstamp order by temps.tstamp desc limit 1");
   $result = mysqli_fetch_assoc($query);
 
   echo(json_encode($result));
