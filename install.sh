0523578e7d37ecc96aca0d16d3725dd1c9626f8b
Daniel Bacher <danielbacher@phill93.de>
2016-06-29 15:09:13 +0200

    Very Final Release

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 
+echo "dtoverlay=dht11,gpiopin=4" >> /boot/config.txt
 apt-get install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt -y
 apt-get install python3 python3-pip -y
 pip3 install pymysql
@@ -10,3 +11,6 @@ systemctl restart mysql
 mysql -p < tools/weather.sql
 echo "*/1 * * * * root python3 ${BASEDIR}/tools/read_sensor.py" > /etc/cron.d/weather
 ln -s ${BASEDIR} /var/www/html/weather
+echo "Rebooting in 5 seconds"
+sleep 5
+reboot
