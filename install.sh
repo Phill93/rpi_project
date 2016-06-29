419552da53028ab5502bdfc9bedd56df1b39cafc
Daniel Bacher <danielbacher@phill93.de>
2016-06-29 15:31:44 +0200

    Error Fix Relative Paths

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ apt-get update
 apt-get install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt -y
 apt-get install python3 python3-pip -y
 pip3 install pymysql
-cp tools/weather.cnf /etc/mysql/conf.d/
+cp ${BASEDIR}/tools/weather.cnf /etc/mysql/conf.d/
 systemctl restart mysql
-mysql -p < tools/weather.sql
+mysql -p < ${BASEDIR}/tools/weather.sql
 echo "*/1 * * * * root python3 ${BASEDIR}/tools/read_sensor.py" > /etc/cron.d/weather
 ln -s ${BASEDIR} /var/www/html/weather
 echo "Rebooting in 5 seconds"
