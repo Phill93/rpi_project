e5a44920aebf69034d92230a1f82d9bfa61afe80
Daniel Bacher <danielbacher@phill93.de>
2016-06-29 15:36:47 +0200

    Add visual warning

--- install.sh
+++ install.sh
@@ -9,7 +9,13 @@ apt-get install python3 python3-pip -y
 pip3 install pymysql
 cp ${BASEDIR}/tools/weather.cnf /etc/mysql/conf.d/
 systemctl restart mysql
-mysql -p < ${BASEDIR}/tools/weather.sql
+
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "           Please enter MySQL root password!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo ""
+mysql -u root -p < ${BASEDIR}/tools/weather.sql
 echo "*/1 * * * * root python3 ${BASEDIR}/tools/read_sensor.py" > /etc/cron.d/weather
 ln -s ${BASEDIR} /var/www/html/weather
 echo "Rebooting in 5 seconds"
